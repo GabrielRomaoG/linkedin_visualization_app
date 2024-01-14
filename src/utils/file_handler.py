b94252be1cd1fe6378686a40f2ecccd8d2838a89
Gabriel Romao <gabriel-rgomes@hotmail.com>
2024-01-14 20:58:47 -0300

    feat: implement extract_files_from_zip function

---
+++ src/utils/file_handler.py
@@ -0,0 +1,10 @@
+from zipfile import ZipFile
+
+
+def extract_files_from_zip(
+    self, file_path: str, destine_path: str, files_to_extract: list
+):
+    with ZipFile(file_path, "r") as zObject:
+        # zObject.extractall(path=destine_path)
+        for file in files_to_extract:
+            zObject.extract(file, path=destine_path)
