cd84fd230d285d8fb0ef6966596bb20fc3699120
Gabriel Romao <gabriel-rgomes@hotmail.com>
2024-01-15 20:20:33 -0300

    feat: implement get_file_name_from_path function

--- src/utils/file_handler.py
+++ src/utils/file_handler.py
@@ -6,3 +6,8 @@ def extract_files_from_zip(file_path: str, destine_path: str, files_to_extract: 
         # zObject.extractall(path=destine_path)
         for file in files_to_extract:
             zObject.extract(file, path=destine_path)
+
+
+def get_file_name_from_path(file_path: str):
+    file_name = file_path.split("/")[-1]
+    return file_name
