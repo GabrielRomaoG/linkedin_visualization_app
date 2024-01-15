842fbfceff7abf68313941bb86b03858dfb6687e
Gabriel Romao <gabriel-rgomes@hotmail.com>
2024-01-14 22:43:12 -0300

    fix: remove self from extract_files_from_zip arguments

--- src/utils/file_handler.py
+++ src/utils/file_handler.py
@@ -1,9 +1,7 @@
 from zipfile import ZipFile
 
 
-def extract_files_from_zip(
-    self, file_path: str, destine_path: str, files_to_extract: list
-):
+def extract_files_from_zip(file_path: str, destine_path: str, files_to_extract: list):
     with ZipFile(file_path, "r") as zObject:
         # zObject.extractall(path=destine_path)
         for file in files_to_extract:
