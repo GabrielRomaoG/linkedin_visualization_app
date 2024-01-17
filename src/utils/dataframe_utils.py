48d4521b6dc096a57993bc16ff9a2caced566a61
Gabriel Romao <gabriel-rgomes@hotmail.com>
2024-01-16 22:38:23 -0300

    refact: improve readability of the validation

--- src/utils/dataframe_utils.py
+++ src/utils/dataframe_utils.py
@@ -2,5 +2,5 @@ import pandas as pd
 
 
 def validate_dataframe_columns(dataframe: pd.DataFrame, expected_columns: list) -> None:
-    if not all(col in dataframe.columns for col in expected_columns):
+    if not set(expected_columns) == set(dataframe.columns):
         raise ValueError("DataFrame does not contain all expected columns")
