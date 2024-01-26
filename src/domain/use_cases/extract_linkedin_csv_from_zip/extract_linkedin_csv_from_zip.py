438e37c5280a1051a5f722506508796b9012ca59
Gabriel Romao <gabriel-rgomes@hotmail.com>
2024-01-26 20:26:36 -0300

    refact: inject extract_files_from_zip as dependency

--- src/domain/use_cases/extract_linkedin_csv_from_zip/extract_linkedin_csv_from_zip.py
+++ src/domain/use_cases/extract_linkedin_csv_from_zip/extract_linkedin_csv_from_zip.py
@@ -2,8 +2,9 @@ from src.utils.file_handler import extract_files_from_zip
 
 
 class LinkedinCsvDataExtractor:
-    def __init__(self):
-        self._interest_files = (
+    def __init__(self, extract_files_zip_func=extract_files_from_zip):
+        self.__extract_files_zip_func = extract_files_zip_func
+        self.__interest_files = (
             "Connections.csv",
             "Shares.csv",
             "Comments.csv",
@@ -11,7 +12,7 @@ class LinkedinCsvDataExtractor:
         )
 
     def extract(self, zip_path: str, destine_folder: str):
-        extract_files_from_zip(
+        self.__extract_files_zip_func(
             file_path=zip_path,
             destine_path=destine_folder,
             files_to_extract=self._interest_files,
