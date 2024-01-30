0cd8753bacc0f82e4747b038b12f8dcfa90e670a
Gabriel Romao <gabriel-rgomes@hotmail.com>
2024-01-29 23:17:39 -0300

    config: implement connection script for database

---
+++ src/infra/db/settings/connection.py
@@ -0,0 +1,27 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+class DBConnectionHandler:
+    def __init__(self) -> None:
+        self.__connection_string = "{}:///{}".format(
+            "sqlite",
+            "src/data/processed/default_gabriel_data.db",
+        )
+        self.__engine = self.__create_database_engine()
+        self.session = None
+
+    def __create_database_engine(self):
+        engine = create_engine(self.__connection_string)
+        return engine
+
+    def get_engine(self):
+        return self.__engine
+
+    def __enter__(self):
+        session_make = sessionmaker(bind=self.__engine)
+        self.session = session_make()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.session.close()
