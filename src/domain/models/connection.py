1ca43de8b45b7a980dcf8b054f9e537ea95ac5c4
Gabriel Romao <gabriel-rgomes@hotmail.com>
2024-02-08 21:17:40 -0300

    feat: implement __eq__ method and change connected_on data type to date

--- src/domain/models/connection.py
+++ src/domain/models/connection.py
@@ -1,11 +1,11 @@
-from datetime import date, datetime
+from datetime import date
 
 
 class Connection:
     user_name: str
     company: str | None
     position: str | None
-    connected_on: datetime | None
+    connected_on: date | None
 
     def __init__(
         self,
@@ -13,8 +13,18 @@ class Connection:
         company: str | None,
         position: str | None,
         connected_on: date,
-    ):
+    ) -> None:
         self.user_name = user_name
         self.company = company
         self.position = position
         self.connected_on = connected_on
+
+    def __eq__(self, other):
+        if not isinstance(other, Connection):
+            return NotImplemented
+        return (
+            self.user_name == other.user_name
+            and self.company == other.company
+            and self.position == other.position
+            and self.connected_on == other.connected_on
+        )
