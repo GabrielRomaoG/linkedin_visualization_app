392fc20a50b6e1c4edf1c7ad18eb0bd02a4da928
Gabriel Romao <gabriel-rgomes@hotmail.com>
2024-02-10 23:37:40 -0300

    refact: improve test__process_success checking multiple calls

--- src/domain/use_cases/process_connections_csv/test_process_connections_csv.py
+++ src/domain/use_cases/process_connections_csv/test_process_connections_csv.py
@@ -1,6 +1,6 @@
 from datetime import date
 import unittest
-from unittest.mock import MagicMock, mock_open, patch
+from unittest.mock import MagicMock, call, mock_open, patch
 from src.domain.models.connection import Connection
 from src.domain.use_cases.process_connections_csv.process_connections_csv import (
     ConnectionsCsvProcessor,
@@ -79,37 +79,35 @@ class TestConnectionsCsvProcessor(unittest.TestCase):
 
     def test__process_success(self):
         mocked_file_path = "some/place/Connections.csv"
-        mocked_file_content = "First Name,Last Name,Company,URL,Email Address,Position,Connected On\n \
-             John,Doe,MicroTest,www.example.com/johndoe,johndoe@test.com,Tester,01 Jan 2012"
+        mocked_file_content = (
+            "First Name,Last Name,Company,URL,Email Address,Position,Connected On\n"
+            "John,Doe,MicroTest,www.example.com/johndoe,johndoe@test.com,Tester,01 Jan 2012\n"
+            "Alice,Waht,Testbook,www.lnk.com/alicew,alicew@some.com,Designer,24 Feb 2016"
+        )
         with patch.object(
             self.use_case,
             "_ConnectionsCsvProcessor__open_file",
             mock_open(read_data=mocked_file_content),
         ):
-
+            expected_calls_to_insert_connection = [
+                call(
+                    Connection(
+                        user_name="johndoe",
+                        company="MicroTest",
+                        position="Tester",
+                        connected_on=date(2012, 1, 1),
+                    )
+                ),
+                call(
+                    Connection(
+                        user_name="alicew",
+                        company="Testbook",
+                        position="Designer",
+                        connected_on=date(2016, 2, 24),
+                    )
+                ),
+            ]
             self.use_case.process(mocked_file_path)
-            self.use_case._ConnectionsCsvProcessor__connections_repository.insert_connection.assert_called_once_with(
-                Connection(
-                    user_name="johndoe",
-                    company="MicroTest",
-                    position="Tester",
-                    connected_on=date(2012, 1, 1),
-                )
-            )
-
-    def test__process_multiple_connections(self):
-        mocked_file_path = "some/place/Connections.csv"
-        mocked_file_content = "First Name,Last Name,Company,URL,Email Address,Position,Connected On\n \
-             John,Doe,MicroTest,www.example.com/johndoe,johndoe@test.com,Tester,01 Jan 2012\n \
-             Alice,Waht,Testbook,www.lnk.com/alicew,alicew@some.com,Designer,24 Feb 2016"
-        with patch.object(
-            self.use_case,
-            "_ConnectionsCsvProcessor__open_file",
-            mock_open(read_data=mocked_file_content),
-        ):
-
-            self.use_case.process(mocked_file_path)
-            assert (
-                self.use_case._ConnectionsCsvProcessor__connections_repository.insert_connection.call_count
-                == 2
+            self.use_case._ConnectionsCsvProcessor__connections_repository.insert_connection.assert_has_calls(
+                expected_calls_to_insert_connection, any_order=False
             )
