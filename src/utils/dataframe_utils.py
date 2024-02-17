b5a893cb26bedf53fb3dcc2d0d1c729e3d1477de
Gabriel Romao <gabriel-rgomes@hotmail.com>
2024-02-16 21:16:15 -0300

    refact: delete dataframe_utils.py

--- src/utils/dataframe_utils.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-
-def validate_dataframe_columns(dataframe: pd.DataFrame, expected_columns: list) -> None:
-    if not set(expected_columns) == set(dataframe.columns):
-        raise ValueError("DataFrame does not contain all expected columns")
