7dc687aea22beca3f46fe83097b050bd4ab66f07
Gabriel Romao <gabriel-rgomes@hotmail.com>
2024-02-17 19:52:22 -0300

    refact: insert hint for get_all method return

--- src/domain/use_cases/get_all_connections/iget_all_connections.py
+++ src/domain/use_cases/get_all_connections/iget_all_connections.py
@@ -1,11 +1,12 @@
 from abc import ABC, abstractmethod
+import pandas as pd
 
 
 class IAllConnectionsGetter(ABC):
     """Interface for retrieving all connections as a DataFrame."""
 
     @abstractmethod
-    def get_all(self):
+    def get_all(self) -> pd.DataFrame:
         """Retrieves all connections as a pandas DataFrame.
 
         Returns:
