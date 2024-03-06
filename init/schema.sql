423297c6cc7567f6bc775ee3013d7b63b99b7cfd
Gabriel Romao <gabriel-rgomes@hotmail.com>
2024-03-05 23:58:58 -0300

    feat: add shares table

--- init/schema.sql
+++ init/schema.sql
@@ -4,3 +4,10 @@ CREATE TABLE IF NOT EXISTS connections (
     Position VARCHAR(255),
     Connected_On DATE
 );
+
+CREATE TABLE IF NOT EXISTS shares (
+    share_id VARCHAR(21) PRIMARY KEY,
+    shared_date DATE,
+    num_of_comments INT,
+    num_of_likes INT
+)
