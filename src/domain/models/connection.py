da1b1ef7b169086682bacf09d8891ded8336c6e4
Gabriel Romao <gabriel-rgomes@hotmail.com>
2024-03-05 21:14:22 -0300

    refact: turn Connection model class to dataclass

--- src/domain/models/connection.py
+++ src/domain/models/connection.py
@@ -1,30 +1,10 @@
 from datetime import date
+from dataclasses import dataclass
 
 
+@dataclass
 class Connection:
     user_name: str
-    company: str | None
-    position: str | None
-    connected_on: date | None
-
-    def __init__(
-        self,
-        user_name: str | None,
-        company: str | None,
-        position: str | None,
-        connected_on: date,
-    ) -> None:
-        self.user_name = user_name
-        self.company = company
-        self.position = position
-        self.connected_on = connected_on
-
-    def __eq__(self, other):
-        if not isinstance(other, Connection):
-            return NotImplemented
-        return (
-            self.user_name == other.user_name
-            and self.company == other.company
-            and self.position == other.position
-            and self.connected_on == other.connected_on
-        )
+    company: str | None = None
+    position: str | None = None
+    connected_on: date | None = None
