aefabc9ba68f1f30078c7aeaae555041ec1d7a9e
Gabriel Romao <gabriel-rgomes@hotmail.com>
2024-03-05 23:49:28 -0300

    refact: remove unuseful comments

--- src/infra/db/test_resources/build_test_db/build_test_db.py
+++ src/infra/db/test_resources/build_test_db/build_test_db.py
@@ -14,10 +14,8 @@ def build(db_path: str):
         "src/infra/db/test_resources/build_test_db/init_db/init.sql"
     ) as schema_file:
         schema_sql = schema_file.read()
-        # Split the SQL statements into individual statements
         sql_statements = schema_sql.split(";")
         for statement in sql_statements:
-            # Execute each statement
             statement = statement.strip()
             if statement:
                 cursor.execute(statement)
