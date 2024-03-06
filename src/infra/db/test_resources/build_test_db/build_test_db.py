c8cca71f0d1cb459574bfc5c889b3b71d9c4363b
Gabriel Romao <gabriel-rgomes@hotmail.com>
2024-03-06 00:52:03 -0300

    refact: improve build_test_db by importing the schema.sql

--- src/infra/db/test_resources/build_test_db/build_test_db.py
+++ src/infra/db/test_resources/build_test_db/build_test_db.py
@@ -10,15 +10,17 @@ def build(db_path: str):
     conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
 
-    with open(
-        "src/infra/db/test_resources/build_test_db/init_db/init.sql"
-    ) as schema_file:
-        schema_sql = schema_file.read()
-        sql_statements = schema_sql.split(";")
-        for statement in sql_statements:
-            statement = statement.strip()
-            if statement:
-                cursor.execute(statement)
+    init_db_path = "init/schema.sql"
+    mocked_data_path = "src/infra/db/test_resources/build_test_db/mocked_data.sql"
+
+    for file in (init_db_path, mocked_data_path):
+        with open(file) as schema_file:
+            schema_sql = schema_file.read()
+            sql_statements = schema_sql.split(";")
+            for statement in sql_statements:
+                statement = statement.strip()
+                if statement:
+                    cursor.execute(statement)
 
     conn.commit()
     conn.close()
---
+++ src/infra/db/test_resources/build_test_db/mocked_data.sql
@@ -0,0 +1,45 @@
+INSERT INTO
+    connections (user_name, Company, Position, Connected_On)
+VALUES
+    (
+        'john_doe',
+        'ABC Corp',
+        'Software Engineer',
+        '2024-02-10'
+    ),
+    (
+        'jane_smith',
+        'XYZ Inc',
+        'Marketing Manager',
+        '2024-02-11'
+    ),
+    (
+        'bob_jones',
+        '123 Company',
+        'Sales Representative',
+        '2024-02-12'
+    ),
+    (
+        'alice_williams',
+        'Acme Co',
+        'Product Manager',
+        '2024-02-12'
+    ),
+    (
+        'sam_brown',
+        'Tech Solutions',
+        'IT Consultant',
+        '2024-02-13'
+    );
+
+INSERT INTO
+    shares (
+        share_id,
+        shared_date,
+        num_of_comments,
+        num_of_likes
+    )
+VALUES
+    ('share_id_1', '2023-10-26', 10, 25),
+    ('share_id_2', '2023-10-27', 5, 12),
+    ('share_id_3', '2023-10-28', 15, 30);
